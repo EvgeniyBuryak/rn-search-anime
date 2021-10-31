5698742009c5f206714c888ede2fed51f9261134
Jhonah <buryakone@gmail.com>
2021-10-31 12:31:53 +0700

    get a single anime

--- src/screens/home-screens/views/results-list.view.js
+++ src/screens/home-screens/views/results-list.view.js
@@ -4,11 +4,11 @@ import { withNavigation } from 'react-navigation';
 import ResultsDetail from './results-detail.view';
 
 const ResultsList = ({ results, navigation }) => {
-    
+
     const keyExtractor = useCallback( item => item.id.toString(), []);
     const renderItem = useCallback( ({item}) => {
         return (
-            <TouchableOpacity onPress={() => navigation.navigate('ResultsShow')}>
+            <TouchableOpacity onPress={() => navigation.navigate('ResultsShow', { id: item.id })}>
               <ResultsDetail result={item} />
             </TouchableOpacity>
         )}, []);
