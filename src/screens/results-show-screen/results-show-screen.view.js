eb94d3342e3132925906faac60279e95d829e357
Jhonah <buryakone@gmail.com>
2021-11-01 17:18:40 +0700

    обработка результата и его вывод - картинки

--- src/screens/results-show-screen/results-show-screen.view.js
+++ src/screens/results-show-screen/results-show-screen.view.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
+import { getSingleAnime } from '../../api/single-anime.api';
 
 const ResultsShowScreen = ({ navigation }) => {
+    const [results, setResults] = useState();
     const id = navigation.getParam('id');
 
-    console.log(id);
+    const keyExtractor = useCallback( item =>{ item.id.toString() },[]);
+    const renderItem = useCallback(({item})=>{
+        return <Image style={styles.image} source={{ uri: item.attributes.posterImage.medium }} />
+
+    }, []);
+
+    const getResult = async (id) => {        
+          const result = await getSingleAnime(id);
+          setResults(result);
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
 
     return (
         <View>
             <Text>
                 Results Show Screen
             </Text>
+            <FlatList
+                data={results}
+                keyExtractor={keyExtractor}
+                renderItem={renderItem}
+            />
         </View>
     );
 }
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({
+    image: {
+        width: 250,
+        height: 360,
+        borderRadius: 4
+    },
+});
 
 export default ResultsShowScreen;
