837fd4bf58e2667d0df4e95ba54923e8438625be
Jhonah <buryakone@gmail.com>
2021-11-09 00:24:06 +0700

    поиск по введеному значению

--- src/api/anime.api.js
+++ src/api/anime.api.js
@@ -1,18 +1,21 @@
 import axios from 'axios';
 
-const getArrayAnime = async () => {
+const getArrayAnime = async ( term ) => {
 
     try {        
         const requestApiAnime = axios.create({
             baseURL: 'https://kitsu.io/api/edge/anime',
             headers: { 'content-type': 'application/vnd.api+json' },
+            params: {
+                'filter[text]': term,
+            }
         });
 
         const response = await requestApiAnime.get('?page[limit]=5');
-        return response.data.data;
 
+        return response.data.data;
     } catch (error) {
-        throw new ('Ошибка');
+        throw new ('Ошибка Api');
     }
 };
 
