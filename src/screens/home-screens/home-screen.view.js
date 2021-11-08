8c8218f433608dd6bff1d1dd527996cd1e9b7c1a
Jhonah <buryakone@gmail.com>
2021-11-09 00:24:37 +0700

    добавлен компонент поиска

--- src/screens/home-screens/home-screen.view.js
+++ src/screens/home-screens/home-screen.view.js
@@ -1,47 +1,64 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { getArrayAnime } from '../../api/anime.api';
-import Toast from 'react-native-easy-toast';
+//import Toast from 'react-native-easy-toast';
 import ResultsList from './views/results-list.view'
+import SearchBar from './views/search-bar.view';
 
 const HomeScreen = () => {
   const [results, setResults] = useState('');
-  const toastRef = useRef(null);
+  const [term, setTerm] = useState('');
+  //const toastRef = useRef(null);
 
-  const getResults = async () => {
+  /*const getResults = async (searchTerm) => {
     try {      
-      const result = await getArrayAnime();
+      const result = await getArrayAnime(searchTerm);
 
       setResults(result);
 
     } catch (err) {
       console.log("Something wrong");
     }
-  };
+  };*/
+  const getResults = (searchTerm) => {
+    const response = new Promise((resolve, reject) => {
+        const result = getArrayAnime(searchTerm);
+        
+        resolve(result);
+        reject(new Error("Ошибка Home"));
+    });
 
+    response.then( res => setResults(res))
+    .catch((err) => console.log(err));
+  };
+/*
   useEffect(()=>{
 
-    getResults();
+    //getResults(term);
     
     toastRef.current.show('Hello World!', 2000);
-  }, []);
-
+  }, [term]);
+*/
+//<Toast ref={toastRef} position='top' />
   return (
-    <View style={styles.container}>
-      <Toast ref={toastRef} position='top' />
-      <Text>Anime</Text>
+    <>
+      <SearchBar
+        term={term} 
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => getResults(term)}
+      />
       <ResultsList results={results}/>
-    </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  /*container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 15,
-  }
+  }*/
 });
 
 export default HomeScreen;
